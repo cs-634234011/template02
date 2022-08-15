f78705b491f1e765281742a1fa1f059d29d97488
Poriwat Karansanti <634234011@parichat.skru.ac.th>
2022-08-15 10:35:06 +0700

    first commit

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const dbConn = mysql.createConnection({
+  host     : 'localhost',
+  user     : process.env.MYSQL_USER,
+  password : process.env.MYSQL_PASSWORD,
+  database : process.env.MYSQL_DATABASE,
+  port     : process.env.MYSQL_PORT
+  
+});
+dbConn.connect(function(err) {
+  if (err) throw err;
+  console.log("MySQL Database Connected!");
+});
+
+module.exports = dbConn;
